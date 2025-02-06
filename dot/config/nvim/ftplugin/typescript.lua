0aaefe07c8d8255ef21693648f3309362c770941
g <gtmalmgren@gmail.com>
2025-02-06 10:07:21 -0800

    vim: prevent typescript plugin from loading multiple times

--- dot/config/nvim/ftplugin/typescript.lua
+++ dot/config/nvim/ftplugin/typescript.lua
@@ -1,3 +1,12 @@
+-- Short-circuit if already loaded
+if vim.g.ts_ftplugin_loaded then
+  return
+end
+
+-- Mark the plugin as loaded
+vim.g.ts_ftplugin_loaded = true
+
+-- Your LSP setup for TypeScript
 require('lspconfig').ts_ls.setup {
   on_attach = vim.g.on_attach
 }
