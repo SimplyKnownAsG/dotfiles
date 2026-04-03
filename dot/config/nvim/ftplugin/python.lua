7aac0e3487aef860578895fbbe9741d39b63dfe5
g <gtmalmgren@gmail.com>
2026-04-03 13:39:08 -0700

    nvim: use ty for python lsp

--- dot/config/nvim/ftplugin/python.lua
+++ dot/config/nvim/ftplugin/python.lua
@@ -6,12 +6,6 @@ end
 -- Mark the plugin as loaded
 vim.g.py_ftplugin_loaded = true
 
--- enable pylsp
-
--- vim.lsp.enable("pylsp")
-
--- enable ty
-
 vim.lsp.config('ty', {
   cmd = {'uvx', 'ty', 'server'}
 })
